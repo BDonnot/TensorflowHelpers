07736a2ff76a4acc26f4f655f4eafd43b0365488
BDonnot <benjamin.donnot@gmail.com>
2018-02-08 15:53:16 +0100

    adding log emulator to print instead of writing

---
+++ TensorflowHelpers/Emulator.py
@@ -0,0 +1,5 @@
+class EmulLog():
+    def __init__(self):
+        pass
+    def info(self, str):
+        print(str)
--- TensorflowHelpers/__init__.py
+++ TensorflowHelpers/__init__.py
@@ -1,10 +1,16 @@
 from .ANN import DenseLayer, NNFully, ResidualBlock, DenseBlock
+
+from .DataHandler import ExpDataReader, ExpCSVDataReader, ExpData, ExpTFrecordsDataReader  # reading and handling data
+
+from .Graph import ExpGraphOneXOneY, ExpGraph, ComplexGraph  # the tensorflow "main" comptuation graph
+
 from .Experiment import Path
 from .Experiment import TFWriters, ExpLogger  # logging and saving data
-from .DataHandler import ExpDataReader, ExpCSVDataReader, ExpData, ExpTFrecordsDataReader  # reading and handling data
-from .Graph import ExpGraphOneXOneY, ExpGraph, ComplexGraph  # the tensorflow "main" comptuation graph
 from .Experiment import ExpParam, ExpSaverParam  # parameters for an experiment
 from .Experiment import ExpModel  # model you want to use
 from .Experiment import Exp #, ExpManager # the experience
 
 from .Losses import l2, rmse, pinball, pinball_multi_q, sigmoid_cross_entropy_with_logits
+
+from .Emulator import EmulLog
+
